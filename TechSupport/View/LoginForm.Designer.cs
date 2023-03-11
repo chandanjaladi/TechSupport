f2c64f22c6ddd01c92989354b0a4bd44ddfdf552
Chandan Jaladi <cj00216@my.westga.edu>
2023-03-11 18:57:12 -0500

    Login form and Main form are designed. Names for the labels, variable are updated, passwords are masked with "*".

---
+++ TechSupport/View/LoginForm.Designer.cs
@@ -0,0 +1,121 @@
+﻿namespace TechSupport
+{
+    partial class LoginForm
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            label2 = new Label();
+            usernameTextBox = new TextBox();
+            loginButton = new Button();
+            passwordMaskedTextBox = new MaskedTextBox();
+            errorLabel = new Label();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(112, 62);
+            label1.Name = "label1";
+            label1.Size = new Size(66, 15);
+            label1.TabIndex = 0;
+            label1.Text = "Username: ";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(112, 109);
+            label2.Name = "label2";
+            label2.Size = new Size(63, 15);
+            label2.TabIndex = 1;
+            label2.Text = "Password: ";
+            // 
+            // usernameTextBox
+            // 
+            usernameTextBox.Location = new Point(197, 59);
+            usernameTextBox.Name = "usernameTextBox";
+            usernameTextBox.Size = new Size(100, 23);
+            usernameTextBox.TabIndex = 2;
+            // 
+            // loginButton
+            // 
+            loginButton.Location = new Point(154, 159);
+            loginButton.Name = "loginButton";
+            loginButton.Size = new Size(75, 23);
+            loginButton.TabIndex = 5;
+            loginButton.Text = "Login";
+            loginButton.UseVisualStyleBackColor = true;
+            // 
+            // passwordMaskedTextBox
+            // 
+            passwordMaskedTextBox.Location = new Point(197, 106);
+            passwordMaskedTextBox.Name = "passwordMaskedTextBox";
+            passwordMaskedTextBox.PasswordChar = '*';
+            passwordMaskedTextBox.Size = new Size(100, 23);
+            passwordMaskedTextBox.TabIndex = 4;
+            // 
+            // errorLabel
+            // 
+            errorLabel.AutoSize = true;
+            errorLabel.Location = new Point(112, 213);
+            errorLabel.Name = "errorLabel";
+            errorLabel.Size = new Size(32, 15);
+            errorLabel.TabIndex = 6;
+            errorLabel.Text = "Error";
+            errorLabel.Visible = false;
+            // 
+            // LoginForm
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(409, 264);
+            Controls.Add(errorLabel);
+            Controls.Add(passwordMaskedTextBox);
+            Controls.Add(loginButton);
+            Controls.Add(usernameTextBox);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            MaximizeBox = false;
+            MinimizeBox = false;
+            Name = "LoginForm";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Login Form";
+            Load += Form1_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+        private TextBox usernameTextBox;
+        private Button loginButton;
+        private MaskedTextBox passwordMaskedTextBox;
+        private Label errorLabel;
+    }
+}
---
+++ TechSupport/View/MainForm.Designer.cs
@@ -0,0 +1,111 @@
+﻿namespace TechSupport.View
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            username = new Label();
+            logoutLabel = new LinkLabel();
+            addIncidentButton = new Button();
+            searchIncidentButton = new Button();
+            dataGridView1 = new DataGridView();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            SuspendLayout();
+            // 
+            // username
+            // 
+            username.AutoSize = true;
+            username.Location = new Point(509, 9);
+            username.Name = "username";
+            username.Size = new Size(30, 15);
+            username.TabIndex = 0;
+            username.Text = "Jane";
+            // 
+            // logoutLabel
+            // 
+            logoutLabel.AutoSize = true;
+            logoutLabel.Location = new Point(502, 24);
+            logoutLabel.Name = "logoutLabel";
+            logoutLabel.Size = new Size(45, 15);
+            logoutLabel.TabIndex = 1;
+            logoutLabel.TabStop = true;
+            logoutLabel.Text = "Logout";
+            // 
+            // addIncidentButton
+            // 
+            addIncidentButton.Location = new Point(139, 66);
+            addIncidentButton.Name = "addIncidentButton";
+            addIncidentButton.Size = new Size(101, 34);
+            addIncidentButton.TabIndex = 2;
+            addIncidentButton.Text = "Add Incident";
+            addIncidentButton.UseVisualStyleBackColor = true;
+            // 
+            // searchIncidentButton
+            // 
+            searchIncidentButton.Location = new Point(302, 66);
+            searchIncidentButton.Name = "searchIncidentButton";
+            searchIncidentButton.Size = new Size(101, 34);
+            searchIncidentButton.TabIndex = 3;
+            searchIncidentButton.Text = "Search Incident";
+            searchIncidentButton.UseVisualStyleBackColor = true;
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Location = new Point(12, 125);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.RowTemplate.Height = 25;
+            dataGridView1.Size = new Size(527, 150);
+            dataGridView1.TabIndex = 4;
+            // 
+            // MainForm
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(551, 287);
+            Controls.Add(dataGridView1);
+            Controls.Add(searchIncidentButton);
+            Controls.Add(addIncidentButton);
+            Controls.Add(logoutLabel);
+            Controls.Add(username);
+            Name = "MainForm";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Main Form";
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label username;
+        private LinkLabel logoutLabel;
+        private Button addIncidentButton;
+        private Button searchIncidentButton;
+        private DataGridView dataGridView1;
+    }
+}
