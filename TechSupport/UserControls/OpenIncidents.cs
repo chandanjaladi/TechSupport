9e53d674e61857ae3853e38648512bde98d2a81f
Chandan Jaladi <cj00216@my.westga.edu>
2023-03-18 22:22:48 -0400

    Added an exception catch for the opening of incidents

--- TechSupport/UserControls/OpenIncidents.cs
+++ TechSupport/UserControls/OpenIncidents.cs
@@ -22,22 +22,29 @@ namespace TechSupport.UserControls
         /// </summary>
         public void displayOpenIncidents() 
         {
-            openedIncidentsListView.Clear();
-            openedIncidentsListView.View = System.Windows.Forms.View.Details;
-            openedIncidentsListView.Columns.Add("Product Code",120);
-            openedIncidentsListView.Columns.Add("Date Opened",120);
-            openedIncidentsListView.Columns.Add("Customer", 120);
-            openedIncidentsListView.Columns.Add("Technicians", 120);
-            openedIncidentsListView.Columns.Add("Title", 200);
-
-            for (int i = 0; i < _incidentController.GetOpenIncidents().Count; i++) 
+            try
             {
-                ListViewItem lv = new ListViewItem(_incidentController.GetOpenIncidents()[i].ProductCode);
-                lv.SubItems.Add(_incidentController.GetOpenIncidents()[i].DateOpened.ToShortDateString());
-                lv.SubItems.Add(_incidentController.GetOpenIncidents()[i].CustomerName);
-                lv.SubItems.Add(_incidentController.GetOpenIncidents()[i].TechinicianName);
-                lv.SubItems.Add(_incidentController.GetOpenIncidents()[i].Title);
-                openedIncidentsListView.Items.Add(lv);
+                openedIncidentsListView.Clear();
+                openedIncidentsListView.View = System.Windows.Forms.View.Details;
+                openedIncidentsListView.Columns.Add("Product Code", 120);
+                openedIncidentsListView.Columns.Add("Date Opened", 120);
+                openedIncidentsListView.Columns.Add("Customer", 120);
+                openedIncidentsListView.Columns.Add("Technicians", 120);
+                openedIncidentsListView.Columns.Add("Title", 200);
+
+                for (var i = 0; i < _incidentController.GetOpenIncidents().Count; i++)
+                {
+                    var lv = new ListViewItem(_incidentController.GetOpenIncidents()[i].ProductCode);
+                    lv.SubItems.Add(_incidentController.GetOpenIncidents()[i].DateOpened.ToShortDateString());
+                    lv.SubItems.Add(_incidentController.GetOpenIncidents()[i].CustomerName);
+                    lv.SubItems.Add(_incidentController.GetOpenIncidents()[i].TechinicianName);
+                    lv.SubItems.Add(_incidentController.GetOpenIncidents()[i].Title);
+                    openedIncidentsListView.Items.Add(lv);
+                }
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Please connect to Database");
             }
 
         }
